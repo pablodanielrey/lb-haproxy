82631c5ae39c6da3c95c5151dd8c88fdcd88116c
31381082 <emanuel@econo.unlp.edu.ar>
2017-09-22 14:13:51 -0300

    Modifcamos para que se agregue con dominio y path

--- etcd/setear_servidor.sh
+++ etcd/setear_servidor.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #
 #  $1 -- nombre del servicio, ej: usuarios
-#  $2 -- url del servicio, ej: /users
+#  $2 -- url del servicio, ej: sistema.econo.unlp.edu.ar
 #  $3 -- nombre del servidor, ej: server1_v1
 #  $4 -- ip:puerto del servidor, ej:  192.168.0.3:8000
+#  $5 -- path, ej:  /sistema/api
 #
 #
 
-curl http://127.0.0.1:2379/v2/keys/services/$1/location -XPUT -d value="$2" -d ttl="13"
+CURL http://127.0.0.1:2379/v2/keys/services/$1/location -XPUT -d value="$2" -d ttl="13"
+CURL http://127.0.0.1:2379/v2/keys/services/$1/path -XPUT -d value="$5" -d ttl="13"
 curl http://127.0.0.1:2379/v2/keys/services/$1/upstream/"$3" -XPUT -d value="$4" -d ttl="13"
